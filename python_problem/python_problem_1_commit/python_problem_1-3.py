8e8cc530ef2a63f0ad15704e710f69587bd9b468
songkyung <yysk_915@naver.com>
2021-07-06 21:25:56 +0900

    1-3

---
+++ python_problem/python_problem_1_commit/python_problem_1-3.py
@@ -0,0 +1,25 @@
+import re
+
+
+def get_input():
+    while(1):    
+        input_num=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")        
+        
+        num_format = re.compile(r'^\-?[1-9][0-9]*$')
+        it_is = re.match(num_format,input_num)
+
+        if(it_is):
+            input_num=int(input_num)
+            if(1<=input_num<=3):
+                break
+            else:
+                print("1,2,3 중 하나를 입력하세요")
+        else:
+            print("정수를 입력하세요")
+
+    return input_num
+
+
+num=0
+
+get_input()
