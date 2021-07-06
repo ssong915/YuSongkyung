0df3a4fdd4ce8dccdadf152554787064a824578b
songkyung <yysk_915@naver.com>
2021-07-06 21:07:33 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,4 +1,18 @@
+import re
+
 num = 0
 
-num=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+while(1):    
+    input_num=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")        
+    
+    num_format = re.compile(r'^\-?[1-9][0-9]*$')
+    it_is = re.match(num_format,input_num)
 
+    if(it_is):
+        input_num=int(input_num)
+        if(1<=input_num<=3):
+            break
+        else:
+            print("1,2,3 중 하나를 입력하세요")
+    else:
+        print("정수를 입력하세요")
