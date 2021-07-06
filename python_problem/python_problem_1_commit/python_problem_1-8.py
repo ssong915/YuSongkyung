434e98b1e228312bda24e11f624051f4d398384e
songkyung <yysk_915@naver.com>
2021-07-06 21:58:05 +0900

    1-8

---
+++ python_problem/python_problem_1_commit/python_problem_1-8.py
@@ -0,0 +1,55 @@
+import re
+
+
+def get_input():
+    while(1):    
+        input_num=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")        
+        
+        num_format = re.compile(r'^\-?[1-9][0-9]*$')
+        it_is = re.match(num_format,input_num)
+
+        if(it_is):
+            input_num=int(input_num)
+            if(1<=input_num<=3):
+                break
+            else:
+                print("1,2,3 중 하나를 입력하세요")
+        else:
+            print("정수를 입력하세요")
+
+    return input_num
+
+def brGame():        
+    num=0
+    turn=1
+    while (1):
+        if turn%2==1:
+            print_num=get_input()
+            for i in range(print_num):
+                        num += 1
+                        print('playerA :', num)
+                        if num==31:
+                            break
+            turn+=1
+
+        elif turn%2==0:
+            print_num=get_input()
+            for i in range(print_num):
+                        num += 1
+                        print('playerB :', num)                    
+                        if num==31:
+                            break
+            turn+=1
+
+        if num==31:
+            break
+
+
+    if turn%2==1:
+        print("playerA  win!")
+    elif turn%2==0:
+        print("playerB  win!")
+
+brGame()
+
+    
