4aafaaeb3faf762c77f3091008a39e8a7cbe9eb9
Yu songkyung <yysk_915@naver.com>
2021-07-13 23:07:27 +0900

    CSS 예쁘게 만들기

--- djangogilrs/mysite/blog/views.py
+++ djangogilrs/mysite/blog/views.py
@@ -4,6 +4,4 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
-# 요청(request)을 넘겨받아 render메서드를 호출
-# render 메서드를 호출하여 받은(return) blog/post_list.html템플릿을 보여줌
+    return render(request, 'blog/post_list.html', {'posts': posts})
