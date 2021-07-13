6e7be8bc6d54f9eb9faa37c7f032dfbe11103335
Yu songkyung <yysk_915@naver.com>
2021-07-13 15:36:01 +0900

    Django 뷰 만들기

--- djangogilrs/mysite/blog/views.py
+++ djangogilrs/mysite/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+# 요청(request)을 넘겨받아 render메서드를 호출
+# render 메서드를 호출하여 받은(return) blog/post_list.html템플릿을 보여줌
