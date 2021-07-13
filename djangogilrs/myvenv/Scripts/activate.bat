703fe7e0a7d8f38f5f1c679408f84a6d693f0108
Yu songkyung <yysk_915@naver.com>
2021-07-13 14:41:34 +0900

    Django 설치하기

--- djangogilrs/myvenv/Scripts/activate.bat
+++ djangogilrs/myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\yysk_\Desktop\겅부\피로그래밍\과제제출\djangogilrs\djangogirls\myvenv
+set VIRTUAL_ENV=C:\Users\yysk_\Desktop\겅부\피로그래밍\과제제출\djangogilrs\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
