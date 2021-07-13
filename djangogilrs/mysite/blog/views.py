87d24599023810611e7f7fbd2cda36c9b21e0867
Yu songkyung <yysk_915@naver.com>
2021-07-13 22:49:50 +0900

    템플릿 동적 데이터

--- djangogilrs/mysite/blog/views.py
+++ djangogilrs/mysite/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {})
 # 요청(request)을 넘겨받아 render메서드를 호출
 # render 메서드를 호출하여 받은(return) blog/post_list.html템플릿을 보여줌
