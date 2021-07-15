c4169b8ad4bbc67658db078662837a85116552be
Yusongkyung <yysk_915@naver.com>
2021-07-16 01:41:25 +0900

    09 장고 모델

--- AskdjangoBasic/askcompany/shop/models.py
+++ AskdjangoBasic/askcompany/shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name=models.CharField(max_length=100)
+    desc=models.TextField(blank=True) #blank=True: 빈칸도 허용함
+    price=models.PositiveIntegerField() #양수 받아오기
+
+    created_at=models.DateTimeField(auto_now_add=True) #첨 추가
+    updated_at=models.DateTimeField(auto_now=True) #업데이트될때 추가
+
+
