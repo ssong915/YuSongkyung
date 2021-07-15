7aeec2e91fd8f23d93c2362921aade4dc177ea8d
Yusongkyung <yysk_915@naver.com>
2021-07-16 02:04:03 +0900

    10 장고 모델 필드

---
+++ AskdjangoBasic/askcompany/blog/models.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.db import models
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # OneToOneField: 한명의 유저는 한명의 필드
+    blog_url = models.URLField(blank=True)
+
+class Post(models.Model):    
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # ForeignKey(외래키): 다수의 코멘트ok
+    title = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(allow_unicode=True, db_index=True)
+    content = models.TextField(blank=True)
+    image = models.ImageField(blank=True) #Pillow
+    comment_count = models.PositiveIntegerField(default=0) #댓글갯수
+    tag_set = models.ManyToManyField('Tag', blank=True) 
+    #ManyToManyField: 하나의 포스팅은 다수의 태그 가능
+    is_publish = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # ForeignKey: 다수의 코멘트ok
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model): 
+    name = models.CharField(max_length=50, unique=True)
--- AskdjangoBasic/askcompany/shop/models.py
+++ AskdjangoBasic/askcompany/shop/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Item(models.Model):
     name=models.CharField(max_length=100)
-    desc=models.TextField(blank=True) #blank=True: 빈칸도 허용함
+    desc=models.TextField(blank=True) #blank=True: 빈칸도 허용함 (default: False)
     price=models.PositiveIntegerField() #양수 받아오기
 
-    created_at=models.DateTimeField(auto_now_add=True) #첨 추가
+    created_at=models.DateTimeField(auto_now_add=True) #날짜+시간 #첨 추가
     updated_at=models.DateTimeField(auto_now=True) #업데이트될때 추가
 
 
