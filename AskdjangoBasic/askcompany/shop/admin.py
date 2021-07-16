bc7cfcafbe2327781fe88993dea10c6aaf8c1383
Yusongkyung <yysk_915@naver.com>
2021-07-16 13:35:16 +0900

    12 장고 Admin을 통한 데이터 관리

--- AskdjangoBasic/askcompany/shop/admin.py
+++ AskdjangoBasic/askcompany/shop/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+#admin.site.register(Item) #admin페이지에 SHOP-Item 항목 생성
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'short_desc', 'price','is_publish'] #배열 안에 것들 출력
+    list_display_links = ['name'] # name에 링크걸기
+    list_filter = ['is_publish','updated_at']
+    search_fields = ['name'] # name으로 검색하기
+
+    def short_desc(self, item):
+        return item.desc[:20] #desc의 처음 20자만 가져오기
--- AskdjangoBasic/askcompany/shop/models.py
+++ AskdjangoBasic/askcompany/shop/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-
 class Item(models.Model):
-    name=models.CharField(max_length=100)
-    desc=models.TextField(blank=True) #blank=True: 빈칸도 허용함 (default: False)
-    price=models.PositiveIntegerField() #양수 받아오기
+    name = models.CharField(max_length=100)
+    desc = models.TextField(blank=True) #blank=True: 빈칸도 허용함 (default: False)
+    price = models.PositiveIntegerField(default="") #양수 받아오기
+    is_publish = models.BooleanField(default=False) #날짜+시간 #첨 추가
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) #업데이트될때 추가
 
-    created_at=models.DateTimeField(auto_now_add=True) #날짜+시간 #첨 추가
-    updated_at=models.DateTimeField(auto_now=True) #업데이트될때 추가
-
-
+    def __str__(self):
+        # return self.name
+        # return '<{}> {}'.format(self.pk, self.name)
+        return f'<{self.pk}> {self.name}'
