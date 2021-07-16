56b0945b4e7fb8c3d59a2c200baa455c8f1c5ba7
Yusongkyung <yysk_915@naver.com>
2021-07-17 00:12:44 +0900

    16 django-debug-toolbar를 통한 SQL 디버깅

--- AskdjangoBasic/askcompany/askcompany/settings.py
+++ AskdjangoBasic/askcompany/askcompany/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'debug_toolbar',
     'shop',
     'blog',
 ]
@@ -126,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS=['127.0.0.1']
