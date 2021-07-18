54436d1216f0b56b0d2cef29bc3e5413e80cbe23
Yusongkyung <yysk_915@naver.com>
2021-07-19 02:21:41 +0900

    31 다양한 구동환경을 위한 settings / requirements.txt 분기

--- AskdjangoBasic/askcompany/manage.py
+++ AskdjangoBasic/askcompany/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askcompany.settings') #진입점
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askcompany.settings.dev') #진입점
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
