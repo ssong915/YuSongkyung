47e31dc407c1cefb33851ecec99559e2002d4628
Yusongkyung <yysk_915@naver.com>
2021-07-19 22:02:54 +0900

    (중급편) 08 회원가입 시에 next 인자 처리하기

--- AskdjangoBasic/askdjango/askdjango/accounts/views.py
+++ AskdjangoBasic/askdjango/askdjango/accounts/views.py
@@ -20,7 +20,8 @@ def profile(request):
 #         if form.is_valid():
 #             user = form.save()
 #             auth_login(request, user) #자동으로 로그인 처리
-#             return redirect('profile') #로그인 됐으면 프로필화면으로
+#             next_url=request.GET('next') or profile
+#             return redirect(next_url) #로그인 됐으면 프로필화면으로
 #     else:
 #         form = SignupForm()
 #     return render(request, 'accounts/signup.html', {
@@ -41,7 +42,8 @@ class SignupView(CreateView):
     template_name = 'accounts/signup.html'
 
     def get_success_url(self):
-        return resolve_url('profile')
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
 
     def form_valid(self, form):
         user = form.save()
