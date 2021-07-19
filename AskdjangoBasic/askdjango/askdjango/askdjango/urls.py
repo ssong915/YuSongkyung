8baad45daec0c0a0b0605cf2c73391144986a529
Yusongkyung <yysk_915@naver.com>
2021-07-19 18:36:46 +0900

    (중급편) 03 프로필 페이지 구현하기

--- AskdjangoBasic/askdjango/askdjango/askdjango/urls.py
+++ AskdjangoBasic/askdjango/askdjango/askdjango/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls')),
     path('accounts/',include('accounts.urls')),
-    path('',lambda req:redirect('blog:post_list')), #URL Reverse
+    path('', lambda req: redirect('blog:post_list'), name='root'), #URL Reverse #HOME
 ]
